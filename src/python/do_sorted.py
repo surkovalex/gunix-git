aafedded8dbaf9fc8560c0ca4596b55f08d3e936
guojinming <guojinming@mail.sdmc.com>
2017-07-04 20:41:44 +0800

    update the python example

---
+++ src/python/do_sorted.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from operator import itemgetter
+
+students = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+print(sorted(students, key=itemgetter(0)))
+print(sorted(students, key=lambda t: t[1]))
+print(sorted(students, key=itemgetter(1), reverse=True))
+print(sorted(students, key=itemgetter(1)))
+
+
